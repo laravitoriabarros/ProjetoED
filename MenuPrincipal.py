88f32607e00b7dd99369a108e327b96836c8d8ef
laravitoriabarros <96316110+laravitoriabarros@users.noreply.github.com>
2022-05-09 08:53:16 -0300

    Update MenuPrincipal.py

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -1,6 +1,6 @@
-from Matricula import matricular
-from Ajuste import ajustar
-from Reajuste import reajustar
+
+#from Ajuste import ajustar
+#from Reajuste import reajustar
 
 #Esta função é chamada quando o usuário apresenta uma entrada inválida
 def tente_novamente(inicio):
@@ -71,8 +71,9 @@ def menu_Principal(inicio):
     print("                 Menu do Sistema do Estudante\n")
     print("As opções disponíveis são:\n")
     print("1 - Fazer matrícula (apenas para calouros")
-    print("2 - Ajuste")
-    print("3 - Reajuste\n")
+    print("2 - Fazer matrícula (veteranos)")
+    print("3 - Ajuste")
+    print("4 - Reajuste\n")
 
     opcao = input("Digite o número correspondente a sua escolha: ")
 
@@ -95,21 +96,14 @@ def menu_Principal(inicio):
     #Se a entrada for um número inteiro, então...
     if verificacao_numero_inteiro == True:
 
-        #Transformamos a entrada que estaa no formato de escrita em numeral
+        #Transformamos a entrada que está no formato de escrita em numeral
         opcaoConvert = int(opcao)
 
         
         if opcaoConvert == 1:
            
-           matricular("iniciar")
-
-        if opcaoConvert == 2:
-
-            ajustar("iniciar")
-
-        if opcaoConvert == 3:
-
-            reajustar("iniciar")
+           import Matricula
+           Matricula.matricular_calouros("iniciar")
 
 
-menu_Principal("iniciar")
+menu_Principal('iniciar')
